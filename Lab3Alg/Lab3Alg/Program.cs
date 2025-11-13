b8446d321366bbd1b8011da57d44e85ef5dc782c
YbGHFBK <142352084+YbGHFBK@users.noreply.github.com>
2025-11-13 14:46:07 +0300

    Lab3 add input

--- Lab3Alg/Lab3Alg/Program.cs
+++ Lab3Alg/Lab3Alg/Program.cs
@@ -1,8 +1,32 @@
-﻿public static class Program
+﻿using System.Text;
+
+public static class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine(CanTransform("011", "AABB"));
+
+        Console.Write("""
+            1. Ввести вручную
+            2. Сгенерировать
+            Ваш выбор: 
+            """);
+
+        string x = null, y = null;
+
+        switch(MakeChoice(2))
+        {
+            case 1:
+                (x, y) = GetStrings();
+                break;
+
+            case 2:
+                (x, y) = GenerateStrings();
+                Console.WriteLine(x + '\n' + y);
+                break;
+        }
+
+        Console.WriteLine("Результат: " + CanTransform(x, y));
     }
     static bool CanTransform(string x, string y)
     {
@@ -35,5 +59,100 @@
             }
         }
         return dp[n, m];
+    }
+
+    private static int MakeChoice(int rightLimit = -1, int leftLimit = 1)
+    {
+        while (true)
+        {
+            if(!int.TryParse(
+                Console.ReadLine(),
+                out int choice
+                ))
+            {
+                Console.WriteLine("Введите целое число.");
+                continue;
+            }
+
+            if (rightLimit != -1 && (choice > rightLimit || choice < leftLimit))
+                Console.WriteLine("Введите целое число 1-2");
+            else return choice;
+        }
+    }
+
+    private static (string, string) GetStrings()
+    {
+        Console.Write("Введите первую строку из нулей и единиц: ");
+        string x = Console.ReadLine();
+        CheckLine(x, true);
+        Console.Write("Введите вторую строку из A и B: ");
+        string y = Console.ReadLine();
+        CheckLine(y, false);
+
+        return (x, y);
+    }
+
+    private static (string, string) GenerateStrings()
+    {
+        Console.Write("Введите длину первой строки из нулей и единиц: ");
+        int xl = MakeChoice();
+        Console.Write("Введите длину второй строки из A и B: ");
+        int yl = MakeChoice();
+
+        string x = "", y = "";
+
+        var rng = new Random();
+
+        StringBuilder sb = new(xl);
+
+        for (int i = 0; i < xl; i++)
+        {
+            sb.Append(rng.Next(2));
+        }
+
+        x = sb.ToString();
+
+        sb.Clear();
+
+        for (int i = 0; i < yl;  i++)
+        {
+            sb.Append(rng.Next(2) == 1 ? 'A' : 'B');
+        }
+
+        y = sb.ToString();
+
+        return (x, y);
+    }
+
+    private static void CheckLine(string x, bool mode)
+    {
+        bool isCorrect = true;
+        switch(mode)
+        {
+            case true:
+                foreach (char c in x)
+                {
+                    if (c != '0' && c != '1')   
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+                break;
+
+            case false:
+                foreach (char c in x)
+                {
+                    if (c != 'A' && c != 'B')
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+                break;       
+        }
+
+        if (!isCorrect) throw new FormatException("Неверный формат введённой строки");
+        return;
     }
 }
